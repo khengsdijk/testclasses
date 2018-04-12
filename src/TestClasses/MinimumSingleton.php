9dc20e28f51a801c8e980f14c0a48a3e10fb5c5b
Koen Hengsdijk <koenhengsdijk1@gmail.com>
2018-04-12 13:36:11 +0200

    First commit

---
+++ src/TestClasses/MinimumSingleton.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file contains the DatabaseDMLQueryBuilderQueryPartsWithTest class.
+ *
+ * PHP Version 7.0
+ *
+ * @package    benchmarks/src/resources/testClasses
+ * @author     Koen Hengsdijk
+ * @copyright  2012-2018, M2Mobi BV, Amsterdam, The Netherlands
+ */
+namespace TestClasses;
+
+class MinimumSingleton
+{
+    /**
+     * MinimumSingleton constructor.
+     * @param $foo string cool thing
+     */
+    public function __construct()
+    {
+
+    }
+}
